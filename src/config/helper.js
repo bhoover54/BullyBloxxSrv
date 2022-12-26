7efac1cd96ecd46318fd36eab95a6bf9a768c9d5
Afowowe Olumide Victor <volumide42@gmail.com>
2022-12-26 13:22:35 +0100

    refractor

--- src/config/helper.js
+++ src/config/helper.js
@@ -5,12 +5,4 @@ const verifyEmail = (email) => {
   if (regex.test(email)) return true
   return false
 }
-
-export const decodeToken = (reqs) => {
-  let token = reqs.headers.authorization
-  if (!token) return false
-  token = token.split(" ")[1]
-  return jwt_decode(token)
-}
-
 export default verifyEmail
--- src/config/token.js
+++ src/config/token.js
@@ -9,6 +9,7 @@ const authenticate = (req, res, next) => {
     return res.status(401).json({
       "message": "unauthorized"
     })
+
   jwt.verify(token, "token", (err, user) => {
     if (err) {
       return res.status(403).json({
@@ -21,5 +22,12 @@ const authenticate = (req, res, next) => {
   })
 }
 
-const token = { accessToken, authenticate }
+const decodeToken = (reqs) => {
+  let token = reqs.headers.authorization
+  if (!token) return false
+  token = token.split(" ")[1]
+  return jwt_decode(token)
+}
+
+const token = { accessToken, authenticate, decodeToken }
 export default token
