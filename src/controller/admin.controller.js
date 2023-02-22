23b2d2782f03c9a034d961e115d1c2d90568505f
Afowowe Olumide Victor <volumide42@gmail.com>
2023-02-22 21:14:57 +0100

    deny request implementation

--- src/controller/admin.controller.js
+++ src/controller/admin.controller.js
@@ -3,17 +3,19 @@ import School from "../model/school.model.js"
 import Wallet from "../model/wallet.model.js"
 
 const approveSponsor = async (req, res) => {
+  const dt = req?.query?.type ? "denied" : "approved"
   try {
-    const approve = await School.update(
-      { approved: "approved" },
-      { where: { id: req.params.school_id, approved: "pending" } }
-    )
+    const approve = await School.update({ approved: dt }, { where: { id: req.params.school_id, approved: "pending" } })
     if (!approve[0]) {
       return res.status(HTTP.FORBIDDEN).json({
         message: "fail",
         data: { message: "already approved" }
       })
     }
+    if (dt === "denied")
+      return res.status(HTTP.SUCCESS).json({
+        message: "success"
+      })
     await Wallet.create({
       school_id: req.params.school_id,
       balance: 535
